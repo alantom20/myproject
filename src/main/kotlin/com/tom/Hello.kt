a4d5b6c2871bdddb6e92d52585966ee94bd8715f
Chan hao <a01204000@gmail.com>
2020-10-07 10:55:23 +0800

    Java and Kotlin variable,data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -4,6 +4,12 @@ fun main(args: Array<String>) {
    // print("Hello Kotlin")
     val h  = Human()
     h.hello()
+    var age = 19
+    age = 20
+    var height = 1.7f
+    var name : String
+    name = "Hank"
+    println(name)
 }
 class Human{
     fun hello(){
