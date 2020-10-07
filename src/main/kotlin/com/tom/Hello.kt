39642d3343dddbfa4ef02d1ff7c271015029dd50
Chan hao <a01204000@gmail.com>
2020-10-07 10:05:17 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+
+fun main(args: Array<String>) {
+   // print("Hello Kotlin")
+    val h  = Human()
+    h.hello()
+}
+class Human{
+    fun hello(){
+        print("Hello Kotlin")
+    }
+
+
+}
